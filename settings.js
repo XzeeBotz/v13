5012baeaa9b14340bbd60c7872658a7fad599352
XzeeBotz <100888117+XzeeBotz@users.noreply.github.com>
2022-06-24 05:59:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.APIKeys = {
 
 //===> ATUR BIAR KALIAN SENENG
 global.autoReadGc = true //Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
-global.autoReadAll = false //Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
+global.autoReadAll = true //Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
 global.anticall = true //Ubah Ke false Jika Tidak Ingin Bot Blockir Yang Telepon
 global.available = false //Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
 global.autoTyping = false //Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
@@ -22,18 +22,18 @@ global.limitCount = 70 //70 Dah Pas Banh
 global.prefa = ['','.','/','#']
 
 //===> UBAH DISINI
-global.namaowner = 'Ryuuka Team' //Ganti Jadi Namamu
-global.owner = ['6285609233482','6283891921787'] //Kalo Mau Lebih Tambahin Aja
-global.nomerowner = '6285609233482' //Owner Utama Istilahnya
+global.namaowner = 'BOT OFFICIAL WHATSAPP' //Ganti Jadi Namamu
+global.owner = ['6281381685954','6281381685954','6281381685954'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '6281381685954' //Owner Utama Istilahnya
 global.namabot = 'Hiruko Kagetane' //Ganti Jadi Nama Botmu
-global.packname = 'Hiruko Kagetane' //Sticker Weem
+global.packname = 'BOT OFFICIAL WHATSAPP' //Sticker Weem
 global.author = 'By Ryuuka Team' //Sticker Weem
-global.webme = 'https://api-ryubotz.herokuapp.com' //Ganti Sama Web Mu
-global.linkgroup = 'https://chat.whatsapp.com/BhOYdoWNRFC4n4bdsEexlV' //Ganti Sama Link Gc Mu
+global.webme = 'https://xzeebotz.github.io' //Ganti Sama Web Mu
+global.linkgroup = 'https://chat.whatsapp.com/' //Ganti Sama Link Gc Mu
 global.sessionName = 'session'
-global.youtube = 'https://youtube.com/c/RyuukaBotz' //Ganti Sama Link Yt Mu
-global.github = 'https://github.com/R-TEAM94' //Ganti Sama Link Github Mu
-global.instagram = 'https://instagram.com/fandyyy._' //Ganti Sama Link Ig mu
+global.youtube = 'https://youtube.com/c/MrclNotWibu' //Ganti Sama Link Yt Mu
+global.github = 'https://github.com/XzeeBotz' //Ganti Sama Link Github Mu
+global.instagram = 'https://instagram.com/mrclnotwibu' //Ganti Sama Link Ig mu
 
 //===> BACKGROUND SETIAP MENU
 global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
